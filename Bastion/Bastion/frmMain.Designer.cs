0891e51313ba2b7ac1a6d4685832c983ffaddc67
Jan-Justin van Tonder <u15073298@tuks.co.za>
2017-04-18 18:26:36 +0200

    Swapped out Payments tab for Management.

--- Bastion/Bastion/frmMain.Designer.cs
+++ Bastion/Bastion/frmMain.Designer.cs
@@ -30,43 +30,59 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
-            this.tabBastion = new System.Windows.Forms.TabControl();
-            this.tpgGames = new System.Windows.Forms.TabPage();
-            this.tpgRentals = new System.Windows.Forms.TabPage();
-            this.tpgPayments = new System.Windows.Forms.TabPage();
-            this.tpgCustomers = new System.Windows.Forms.TabPage();
             this.tabImages = new System.Windows.Forms.ImageList(this.components);
+            this.tpgCustomers = new System.Windows.Forms.TabPage();
+            this.tpgRentals = new System.Windows.Forms.TabPage();
+            this.tpgGames = new System.Windows.Forms.TabPage();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnAddGame = new System.Windows.Forms.Button();
+            this.btnUpdateGame = new System.Windows.Forms.Button();
+            this.btnDeleteGame = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.tabBastion.SuspendLayout();
+            this.tabBastion = new System.Windows.Forms.TabControl();
+            this.tpgManagement = new System.Windows.Forms.TabPage();
             this.tpgGames.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabBastion.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tabBastion
+            // tabImages
             // 
-            this.tabBastion.Controls.Add(this.tpgGames);
-            this.tabBastion.Controls.Add(this.tpgRentals);
-            this.tabBastion.Controls.Add(this.tpgPayments);
-            this.tabBastion.Controls.Add(this.tpgCustomers);
-            this.tabBastion.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabBastion.ImageList = this.tabImages;
-            this.tabBastion.ItemSize = new System.Drawing.Size(83, 45);
-            this.tabBastion.Location = new System.Drawing.Point(0, 0);
-            this.tabBastion.Name = "tabBastion";
-            this.tabBastion.SelectedIndex = 0;
-            this.tabBastion.Size = new System.Drawing.Size(1350, 729);
-            this.tabBastion.TabIndex = 0;
+            this.tabImages.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("tabImages.ImageStream")));
+            this.tabImages.TransparentColor = System.Drawing.Color.Transparent;
+            this.tabImages.Images.SetKeyName(0, "gamepad.png");
+            this.tabImages.Images.SetKeyName(1, "coins.png");
+            this.tabImages.Images.SetKeyName(2, "meeting.png");
+            this.tabImages.Images.SetKeyName(3, "manage.png");
+            // 
+            // tpgCustomers
+            // 
+            this.tpgCustomers.ImageIndex = 2;
+            this.tpgCustomers.Location = new System.Drawing.Point(4, 49);
+            this.tpgCustomers.Name = "tpgCustomers";
+            this.tpgCustomers.Padding = new System.Windows.Forms.Padding(3);
+            this.tpgCustomers.Size = new System.Drawing.Size(1342, 676);
+            this.tpgCustomers.TabIndex = 3;
+            this.tpgCustomers.Text = "Customers";
+            this.tpgCustomers.UseVisualStyleBackColor = true;
+            // 
+            // tpgRentals
+            // 
+            this.tpgRentals.ImageIndex = 1;
+            this.tpgRentals.Location = new System.Drawing.Point(4, 49);
+            this.tpgRentals.Name = "tpgRentals";
+            this.tpgRentals.Padding = new System.Windows.Forms.Padding(3);
+            this.tpgRentals.Size = new System.Drawing.Size(1342, 676);
+            this.tpgRentals.TabIndex = 1;
+            this.tpgRentals.Text = "Rentals";
+            this.tpgRentals.UseVisualStyleBackColor = true;
             // 
             // tpgGames
             // 
             this.tpgGames.Controls.Add(this.button4);
-            this.tpgGames.Controls.Add(this.button3);
-            this.tpgGames.Controls.Add(this.button2);
-            this.tpgGames.Controls.Add(this.button1);
+            this.tpgGames.Controls.Add(this.btnDeleteGame);
+            this.tpgGames.Controls.Add(this.btnUpdateGame);
+            this.tpgGames.Controls.Add(this.btnAddGame);
             this.tpgGames.Controls.Add(this.dataGridView1);
             this.tpgGames.ImageIndex = 0;
             this.tpgGames.Location = new System.Drawing.Point(4, 49);
@@ -77,48 +93,6 @@
             this.tpgGames.Text = "Games";
             this.tpgGames.UseVisualStyleBackColor = true;
             // 
-            // tpgRentals
-            // 
-            this.tpgRentals.ImageIndex = 1;
-            this.tpgRentals.Location = new System.Drawing.Point(4, 49);
-            this.tpgRentals.Name = "tpgRentals";
-            this.tpgRentals.Padding = new System.Windows.Forms.Padding(3);
-            this.tpgRentals.Size = new System.Drawing.Size(894, 360);
-            this.tpgRentals.TabIndex = 1;
-            this.tpgRentals.Text = "Rentals";
-            this.tpgRentals.UseVisualStyleBackColor = true;
-            // 
-            // tpgPayments
-            // 
-            this.tpgPayments.ImageIndex = 2;
-            this.tpgPayments.Location = new System.Drawing.Point(4, 49);
-            this.tpgPayments.Name = "tpgPayments";
-            this.tpgPayments.Padding = new System.Windows.Forms.Padding(3);
-            this.tpgPayments.Size = new System.Drawing.Size(894, 360);
-            this.tpgPayments.TabIndex = 2;
-            this.tpgPayments.Text = "Payments";
-            this.tpgPayments.UseVisualStyleBackColor = true;
-            // 
-            // tpgCustomers
-            // 
-            this.tpgCustomers.ImageIndex = 3;
-            this.tpgCustomers.Location = new System.Drawing.Point(4, 49);
-            this.tpgCustomers.Name = "tpgCustomers";
-            this.tpgCustomers.Padding = new System.Windows.Forms.Padding(3);
-            this.tpgCustomers.Size = new System.Drawing.Size(894, 360);
-            this.tpgCustomers.TabIndex = 3;
-            this.tpgCustomers.Text = "Customers";
-            this.tpgCustomers.UseVisualStyleBackColor = true;
-            // 
-            // tabImages
-            // 
-            this.tabImages.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("tabImages.ImageStream")));
-            this.tabImages.TransparentColor = System.Drawing.Color.Transparent;
-            this.tabImages.Images.SetKeyName(0, "gamepad.png");
-            this.tabImages.Images.SetKeyName(1, "rent.png");
-            this.tabImages.Images.SetKeyName(2, "coins.png");
-            this.tabImages.Images.SetKeyName(3, "meeting.png");
-            // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
@@ -127,32 +101,32 @@
             this.dataGridView1.Size = new System.Drawing.Size(1067, 591);
             this.dataGridView1.TabIndex = 0;
             // 
-            // button1
+            // btnAddGame
             // 
-            this.button1.Location = new System.Drawing.Point(8, 77);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(253, 50);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnAddGame.Location = new System.Drawing.Point(8, 77);
+            this.btnAddGame.Name = "btnAddGame";
+            this.btnAddGame.Size = new System.Drawing.Size(253, 50);
+            this.btnAddGame.TabIndex = 1;
+            this.btnAddGame.Text = "Add Game";
+            this.btnAddGame.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnUpdateGame
             // 
-            this.button2.Location = new System.Drawing.Point(8, 133);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(253, 50);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnUpdateGame.Location = new System.Drawing.Point(8, 133);
+            this.btnUpdateGame.Name = "btnUpdateGame";
+            this.btnUpdateGame.Size = new System.Drawing.Size(253, 50);
+            this.btnUpdateGame.TabIndex = 2;
+            this.btnUpdateGame.Text = "Update Game";
+            this.btnUpdateGame.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnDeleteGame
             // 
-            this.button3.Location = new System.Drawing.Point(8, 189);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(253, 50);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnDeleteGame.Location = new System.Drawing.Point(8, 189);
+            this.btnDeleteGame.Name = "btnDeleteGame";
+            this.btnDeleteGame.Size = new System.Drawing.Size(253, 50);
+            this.btnDeleteGame.TabIndex = 3;
+            this.btnDeleteGame.Text = "Delete Game";
+            this.btnDeleteGame.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
@@ -162,6 +136,32 @@
             this.button4.TabIndex = 4;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
+            // 
+            // tabBastion
+            // 
+            this.tabBastion.Controls.Add(this.tpgGames);
+            this.tabBastion.Controls.Add(this.tpgRentals);
+            this.tabBastion.Controls.Add(this.tpgCustomers);
+            this.tabBastion.Controls.Add(this.tpgManagement);
+            this.tabBastion.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabBastion.ImageList = this.tabImages;
+            this.tabBastion.ItemSize = new System.Drawing.Size(83, 45);
+            this.tabBastion.Location = new System.Drawing.Point(0, 0);
+            this.tabBastion.Name = "tabBastion";
+            this.tabBastion.SelectedIndex = 0;
+            this.tabBastion.Size = new System.Drawing.Size(1350, 729);
+            this.tabBastion.TabIndex = 0;
+            // 
+            // tpgManagement
+            // 
+            this.tpgManagement.ImageIndex = 3;
+            this.tpgManagement.Location = new System.Drawing.Point(4, 49);
+            this.tpgManagement.Name = "tpgManagement";
+            this.tpgManagement.Padding = new System.Windows.Forms.Padding(3);
+            this.tpgManagement.Size = new System.Drawing.Size(1342, 676);
+            this.tpgManagement.TabIndex = 4;
+            this.tpgManagement.Text = "Management";
+            this.tpgManagement.UseVisualStyleBackColor = true;
             // 
             // frmMain
             // 
@@ -173,26 +173,25 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmMain";
             this.Text = "BG";
-            this.tabBastion.ResumeLayout(false);
             this.tpgGames.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabBastion.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TabControl tabBastion;
-        private System.Windows.Forms.TabPage tpgGames;
-        private System.Windows.Forms.TabPage tpgRentals;
-        private System.Windows.Forms.TabPage tpgPayments;
-        private System.Windows.Forms.TabPage tpgCustomers;
         private System.Windows.Forms.ImageList tabImages;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabPage tpgCustomers;
+        private System.Windows.Forms.TabPage tpgRentals;
+        private System.Windows.Forms.TabPage tpgGames;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnDeleteGame;
+        private System.Windows.Forms.Button btnUpdateGame;
+        private System.Windows.Forms.Button btnAddGame;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabControl tabBastion;
+        private System.Windows.Forms.TabPage tpgManagement;
     }
 }
 
