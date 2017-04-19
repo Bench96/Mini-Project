3802da200c9c8fff25668ba91afe1cdfa05d1858
Munyaradzi Mpofu <munyabenyera@gmail.com>
2017-04-19 17:36:17 +0200

    Started with teh Games section

--- Bastion/Bastion/frmMain.Designer.cs
+++ Bastion/Bastion/frmMain.Designer.cs
@@ -34,15 +34,33 @@
             this.tpgCustomers = new System.Windows.Forms.TabPage();
             this.tpgRentals = new System.Windows.Forms.TabPage();
             this.tpgGames = new System.Windows.Forms.TabPage();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.btnAddGame = new System.Windows.Forms.Button();
-            this.btnUpdateGame = new System.Windows.Forms.Button();
-            this.btnDeleteGame = new System.Windows.Forms.Button();
+            this.clearSearchResultsBTN = new System.Windows.Forms.Button();
+            this.gameSearchTextBox = new System.Windows.Forms.TextBox();
+            this.gameSearchBTN = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.btnDeleteGame = new System.Windows.Forms.Button();
+            this.btnUpdateGame = new System.Windows.Forms.Button();
+            this.btnAddGame = new System.Windows.Forms.Button();
+            this.gameList = new System.Windows.Forms.DataGridView();
+            this.addGameGroup = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.addGameBTN = new System.Windows.Forms.Button();
+            this.gameTitleTextBox = new System.Windows.Forms.TextBox();
+            this.gamePublisherComboBox = new System.Windows.Forms.ComboBox();
+            this.gameReleaseYearTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.gameRentalRateTextBox = new System.Windows.Forms.TextBox();
+            this.gameGenreComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.gameDeveloperComboBox = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.tabBastion = new System.Windows.Forms.TabControl();
             this.tpgManagement = new System.Windows.Forms.TabPage();
             this.tpgGames.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gameList)).BeginInit();
+            this.addGameGroup.SuspendLayout();
             this.tabBastion.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -79,11 +97,15 @@
             // 
             // tpgGames
             // 
+            this.tpgGames.Controls.Add(this.clearSearchResultsBTN);
+            this.tpgGames.Controls.Add(this.gameSearchTextBox);
+            this.tpgGames.Controls.Add(this.gameSearchBTN);
             this.tpgGames.Controls.Add(this.button4);
             this.tpgGames.Controls.Add(this.btnDeleteGame);
             this.tpgGames.Controls.Add(this.btnUpdateGame);
             this.tpgGames.Controls.Add(this.btnAddGame);
-            this.tpgGames.Controls.Add(this.dataGridView1);
+            this.tpgGames.Controls.Add(this.addGameGroup);
+            this.tpgGames.Controls.Add(this.gameList);
             this.tpgGames.ImageIndex = 0;
             this.tpgGames.Location = new System.Drawing.Point(4, 49);
             this.tpgGames.Name = "tpgGames";
@@ -93,31 +115,41 @@
             this.tpgGames.Text = "Games";
             this.tpgGames.UseVisualStyleBackColor = true;
             // 
-            // dataGridView1
+            // clearSearchResultsBTN
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(267, 77);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(1067, 591);
-            this.dataGridView1.TabIndex = 0;
+            this.clearSearchResultsBTN.Location = new System.Drawing.Point(604, 28);
+            this.clearSearchResultsBTN.Name = "clearSearchResultsBTN";
+            this.clearSearchResultsBTN.Size = new System.Drawing.Size(132, 23);
+            this.clearSearchResultsBTN.TabIndex = 7;
+            this.clearSearchResultsBTN.Text = "Clear Search Results";
+            this.clearSearchResultsBTN.UseVisualStyleBackColor = true;
+            this.clearSearchResultsBTN.Click += new System.EventHandler(this.clearSearchResultsBTN_Click);
             // 
-            // btnAddGame
+            // gameSearchTextBox
             // 
-            this.btnAddGame.Location = new System.Drawing.Point(8, 77);
-            this.btnAddGame.Name = "btnAddGame";
-            this.btnAddGame.Size = new System.Drawing.Size(253, 50);
-            this.btnAddGame.TabIndex = 1;
-            this.btnAddGame.Text = "Add Game";
-            this.btnAddGame.UseVisualStyleBackColor = true;
+            this.gameSearchTextBox.Location = new System.Drawing.Point(276, 31);
+            this.gameSearchTextBox.Name = "gameSearchTextBox";
+            this.gameSearchTextBox.Size = new System.Drawing.Size(175, 20);
+            this.gameSearchTextBox.TabIndex = 6;
             // 
-            // btnUpdateGame
+            // gameSearchBTN
             // 
-            this.btnUpdateGame.Location = new System.Drawing.Point(8, 133);
-            this.btnUpdateGame.Name = "btnUpdateGame";
-            this.btnUpdateGame.Size = new System.Drawing.Size(253, 50);
-            this.btnUpdateGame.TabIndex = 2;
-            this.btnUpdateGame.Text = "Update Game";
-            this.btnUpdateGame.UseVisualStyleBackColor = true;
+            this.gameSearchBTN.Location = new System.Drawing.Point(486, 28);
+            this.gameSearchBTN.Name = "gameSearchBTN";
+            this.gameSearchBTN.Size = new System.Drawing.Size(76, 23);
+            this.gameSearchBTN.TabIndex = 5;
+            this.gameSearchBTN.Text = "Search";
+            this.gameSearchBTN.UseVisualStyleBackColor = true;
+            this.gameSearchBTN.Click += new System.EventHandler(this.gameSearchBTN_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(8, 245);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(253, 50);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // btnDeleteGame
             // 
@@ -128,14 +160,163 @@
             this.btnDeleteGame.Text = "Delete Game";
             this.btnDeleteGame.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btnUpdateGame
             // 
-            this.button4.Location = new System.Drawing.Point(8, 245);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(253, 50);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnUpdateGame.Location = new System.Drawing.Point(8, 133);
+            this.btnUpdateGame.Name = "btnUpdateGame";
+            this.btnUpdateGame.Size = new System.Drawing.Size(253, 50);
+            this.btnUpdateGame.TabIndex = 2;
+            this.btnUpdateGame.Text = "Update Game";
+            this.btnUpdateGame.UseVisualStyleBackColor = true;
+            // 
+            // btnAddGame
+            // 
+            this.btnAddGame.Location = new System.Drawing.Point(8, 77);
+            this.btnAddGame.Name = "btnAddGame";
+            this.btnAddGame.Size = new System.Drawing.Size(253, 50);
+            this.btnAddGame.TabIndex = 1;
+            this.btnAddGame.Text = "Add Game";
+            this.btnAddGame.UseVisualStyleBackColor = true;
+            this.btnAddGame.Click += new System.EventHandler(this.btnAddGame_Click);
+            // 
+            // gameList
+            // 
+            this.gameList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gameList.Location = new System.Drawing.Point(276, 77);
+            this.gameList.Name = "gameList";
+            this.gameList.Size = new System.Drawing.Size(868, 357);
+            this.gameList.TabIndex = 0;
+            // 
+            // addGameGroup
+            // 
+            this.addGameGroup.Controls.Add(this.label1);
+            this.addGameGroup.Controls.Add(this.addGameBTN);
+            this.addGameGroup.Controls.Add(this.gameTitleTextBox);
+            this.addGameGroup.Controls.Add(this.gamePublisherComboBox);
+            this.addGameGroup.Controls.Add(this.gameReleaseYearTextBox);
+            this.addGameGroup.Controls.Add(this.label6);
+            this.addGameGroup.Controls.Add(this.gameRentalRateTextBox);
+            this.addGameGroup.Controls.Add(this.gameGenreComboBox);
+            this.addGameGroup.Controls.Add(this.label2);
+            this.addGameGroup.Controls.Add(this.label5);
+            this.addGameGroup.Controls.Add(this.label3);
+            this.addGameGroup.Controls.Add(this.gameDeveloperComboBox);
+            this.addGameGroup.Controls.Add(this.label4);
+            this.addGameGroup.Location = new System.Drawing.Point(301, 77);
+            this.addGameGroup.Name = "addGameGroup";
+            this.addGameGroup.Size = new System.Drawing.Size(303, 409);
+            this.addGameGroup.TabIndex = 24;
+            this.addGameGroup.TabStop = false;
+            this.addGameGroup.Text = "Add Game";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Title";
+            // 
+            // addGameBTN
+            // 
+            this.addGameBTN.Location = new System.Drawing.Point(58, 355);
+            this.addGameBTN.Name = "addGameBTN";
+            this.addGameBTN.Size = new System.Drawing.Size(75, 23);
+            this.addGameBTN.TabIndex = 23;
+            this.addGameBTN.Text = "Add Game";
+            this.addGameBTN.UseVisualStyleBackColor = true;
+            this.addGameBTN.Click += new System.EventHandler(this.addGameBTN_Click);
+            // 
+            // gameTitleTextBox
+            // 
+            this.gameTitleTextBox.Location = new System.Drawing.Point(58, 34);
+            this.gameTitleTextBox.Name = "gameTitleTextBox";
+            this.gameTitleTextBox.Size = new System.Drawing.Size(175, 20);
+            this.gameTitleTextBox.TabIndex = 8;
+            // 
+            // gamePublisherComboBox
+            // 
+            this.gamePublisherComboBox.FormattingEnabled = true;
+            this.gamePublisherComboBox.Location = new System.Drawing.Point(58, 317);
+            this.gamePublisherComboBox.Name = "gamePublisherComboBox";
+            this.gamePublisherComboBox.Size = new System.Drawing.Size(175, 21);
+            this.gamePublisherComboBox.TabIndex = 22;
+            // 
+            // gameReleaseYearTextBox
+            // 
+            this.gameReleaseYearTextBox.Location = new System.Drawing.Point(58, 90);
+            this.gameReleaseYearTextBox.Name = "gameReleaseYearTextBox";
+            this.gameReleaseYearTextBox.Size = new System.Drawing.Size(175, 20);
+            this.gameReleaseYearTextBox.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(55, 302);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Publisher";
+            // 
+            // gameRentalRateTextBox
+            // 
+            this.gameRentalRateTextBox.Location = new System.Drawing.Point(58, 146);
+            this.gameRentalRateTextBox.Name = "gameRentalRateTextBox";
+            this.gameRentalRateTextBox.Size = new System.Drawing.Size(175, 20);
+            this.gameRentalRateTextBox.TabIndex = 13;
+            // 
+            // gameGenreComboBox
+            // 
+            this.gameGenreComboBox.FormattingEnabled = true;
+            this.gameGenreComboBox.Location = new System.Drawing.Point(58, 258);
+            this.gameGenreComboBox.Name = "gameGenreComboBox";
+            this.gameGenreComboBox.Size = new System.Drawing.Size(175, 21);
+            this.gameGenreComboBox.TabIndex = 20;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Release Year";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(55, 243);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Genre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Rental Rate";
+            // 
+            // gameDeveloperComboBox
+            // 
+            this.gameDeveloperComboBox.FormattingEnabled = true;
+            this.gameDeveloperComboBox.Location = new System.Drawing.Point(58, 202);
+            this.gameDeveloperComboBox.Name = "gameDeveloperComboBox";
+            this.gameDeveloperComboBox.Size = new System.Drawing.Size(175, 21);
+            this.gameDeveloperComboBox.TabIndex = 18;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(55, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Developer";
             // 
             // tabBastion
             // 
@@ -174,7 +355,10 @@
             this.Name = "frmMain";
             this.Text = "BG";
             this.tpgGames.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tpgGames.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gameList)).EndInit();
+            this.addGameGroup.ResumeLayout(false);
+            this.addGameGroup.PerformLayout();
             this.tabBastion.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -189,9 +373,26 @@
         private System.Windows.Forms.Button btnDeleteGame;
         private System.Windows.Forms.Button btnUpdateGame;
         private System.Windows.Forms.Button btnAddGame;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView gameList;
         private System.Windows.Forms.TabControl tabBastion;
         private System.Windows.Forms.TabPage tpgManagement;
+        private System.Windows.Forms.TextBox gameSearchTextBox;
+        private System.Windows.Forms.Button gameSearchBTN;
+        private System.Windows.Forms.Button clearSearchResultsBTN;
+        private System.Windows.Forms.TextBox gameRentalRateTextBox;
+        private System.Windows.Forms.TextBox gameReleaseYearTextBox;
+        private System.Windows.Forms.TextBox gameTitleTextBox;
+        private System.Windows.Forms.ComboBox gamePublisherComboBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox gameGenreComboBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox gameDeveloperComboBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox addGameGroup;
+        private System.Windows.Forms.Button addGameBTN;
     }
 }
 
--- Bastion/Bastion/frmMain.cs
+++ Bastion/Bastion/frmMain.cs
@@ -12,9 +12,65 @@ namespace Bastion
 {
     public partial class frmMain : Form
     {
+        Models.GameRentalsEntities db = new Models.GameRentalsEntities();
+
         public frmMain()
         {
             InitializeComponent();
+
+            var games = db.Games.ToList();
+            var developers = db.Developers.ToList();
+            //foreach(Models.Game game in games)
+            //{
+            //    game.Developers = Where(u => u.Developers.Any(v => v.DeveloperID))
+            //}
+            //gameList.DataSource = db.Games.ToList();
+            //foreach(Models.Game game in gameList)
+            //{
+
+            //}
+            addGameGroup.Hide();
+        }
+
+        //***************************************Games Section********************************************//
+
+        private void gameSearchBTN_Click(object sender, EventArgs e)
+        {
+            string searchQuery = gameSearchTextBox.Text;
+
+            if(searchQuery != null)
+            {
+                gameList.DataSource = db.Games.Where(u => u.Title.ToLower().Contains(searchQuery.ToLower())).ToList();
+            }
+        }
+
+        private void clearSearchResultsBTN_Click(object sender, EventArgs e)
+        {
+            gameList.DataSource = db.Games.ToList();
+        }
+
+        private void btnAddGame_Click(object sender, EventArgs e)
+        {
+            gameList.Hide();
+            addGameGroup.Show();
+            
+            //populate combo boxes
+            gameDeveloperComboBox.DataSource = db.Developers.Select(u => u.Name).ToList();
+            gameGenreComboBox.DataSource = db.Genres.Select(u => u.Name).ToList();
+            gamePublisherComboBox.DataSource = db.Publishers.Select(u => u.Name).ToList();
+        }
+
+        private void addGameBTN_Click(object sender, EventArgs e)
+        {
+            if(gameTitleTextBox.Text == "" || gameReleaseYearTextBox.Text == "" || gameRentalRateTextBox.Text == "")
+            {
+                MessageBox.Show("Please complete all the fields");
+            }
+            else
+            {
+                gameList.Show();
+                addGameGroup.Hide();
+            }   
         }
     }
 }
